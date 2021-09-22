6818b70e8d0f8680b8f675a6e886a4b346272680
Shirley Leung <sleung@idmod.org>
2021-09-22 00:41:57 -0700

    added many new time series tabs

--- dash_elim_prob_matrices_prev_ts_tabs.py
+++ dash_elim_prob_matrices_prev_ts_tabs.py
@@ -28,6 +28,7 @@ wi_name_sh = 'spatial, classic drive, VC and GM, EIR = 30'
 distrib_itns = True
 num_sweep_vars = 4
 drive_type = 'classic'
+effector_allele = 'a1'
 
 # -- 3.) Spatial, integral, GM only, EIR = 30
 # wi_name = 'spatialinside_integral2l4a_GM_only_aEIR30_sweep_rc_d1_rr20_se2'
@@ -60,34 +61,35 @@ num_seeds = 20  # num of seeds per sim
 # NOTE: all value arrays must be sorted increasing
 if num_sweep_vars == 6:
     if drive_type == 'classic':
-        allvardefs = {'rc': 1, 'd': 1, 'rr0': 0, 'sne': 0,
-                      'rd': 180, 'nn': 6}
-        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1],
-                      'd': [0.9, 0.95, 1],
-                      'rr0': [0, 0.001, 0.01, 0.1],
-                      'sne': [0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5],
+        allvardefs = {'rc': 1.0, 'd': 1.0, 'rr0': 0.0, 'sne': 0.0,
+                      'rd': 180.0, 'nn': 6.0}
+        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                      'd': [0.9, 0.95, 1.0],
+                      'rr0': [0.0, 0.001, 0.01, 0.1],
+                      'sne': [0.0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5],
                       # 'rd': [180, 240, 300, 360, 420, 480, 545],
-                      'rd': [180],
-                      'nn': [6]}
+                      'rd': [180.0],
+                      'nn': [6.0]}
         # 'nn': [6, 12]}
 elif num_sweep_vars == 4:
     if drive_type == 'classic':
-        allvardefs = {'rc': 1, 'd': 1, 'sne': 0, 'rr0': 0}
-        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1],
-                      'd': [0.9, 0.95, 1],
-                      'rr0': [0, 0.001, 0.01, 0.1],
-                      'sne': [0, 0.1, 0.2, 0.3, 0.4, 0.5]}
-                      # 'sne': [0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]}
+        allvardefs = {'rc': 1.0, 'd': 1.0, 'sne': 0.0, 'rr0': 0.0}
+        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                      'd': [0.9, 0.95, 1.0],
+                      'rr0': [0.0, 0.001, 0.01, 0.1],
+                      'sne': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]}
+                      # 'sne': [0.0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]}
     elif drive_type == 'integral':
-        allvardefs = {'rc': 1, 'd1': 1, 'se2': 0, 'rr20': 0}
-        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1],
-                      'd1': [0.9, 0.95, 1],
-                      'rr20': [0, 0.001, 0.01, 0.1],
-                      'se2': [0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]}
+        allvardefs = {'rc': 1.0, 'd1': 1.0, 'se2': 0.0, 'rr20': 0.0}
+        allvarvals = {'rc': [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                      'd1': [0.9, 0.95, 1.0],
+                      'rr20': [0.0, 0.001, 0.01, 0.1],
+                      'se2': [0.0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]}
 
 ##
 # -------- Load data
 dfi = pd.read_csv(os.path.join(data_dir, 'dfi_' + wi_name + '.csv'))
+dfi['Infectious Vectors Num'] = dfi['Adult Vectors'] * dfi['Infectious Vectors']
 dfa = pd.read_csv(os.path.join(data_dir, 'dfa_' + wi_name + '.csv'))
 dfe = pd.read_csv(os.path.join(data_dir, 'dfe_' + wi_name + '.csv'))
 dfed = pd.read_csv(os.path.join(data_dir, 'dfed_' + wi_name + '.csv'))
@@ -96,7 +98,7 @@ dfed = pd.read_csv(os.path.join(data_dir, 'dfed_' + wi_name + '.csv'))
 # -------- Dash
 app.layout = html.Div([
     dcc.Tabs([
-        dcc.Tab(label='Elimination probabilities', children=[
+        dcc.Tab(label='Elimination probability matrices', children=[
 
             html.H2(children='Elim probabilities: ' + wi_name_sh),
 
@@ -144,7 +146,7 @@ app.layout = html.Div([
             ])
         ]),
 
-        dcc.Tab(label='Years to elimination', children=[
+        dcc.Tab(label='Years to elimination matrices', children=[
 
             html.H2(children='Years to elim: ' + wi_name_sh),
 
@@ -192,7 +194,7 @@ app.layout = html.Div([
             ])
         ]),
 
-        dcc.Tab(label='Prevalence time series', children=[
+        dcc.Tab(label='PfHRP2 prevalence time series', children=[
 
             html.H2(children='Prev time series: ' + wi_name_sh),
 
@@ -238,9 +240,200 @@ app.layout = html.Div([
                 dcc.Graph(id='prev-ts',
                           style={'width': '100%', 'height': '80vh'})
             ])
+        ]),
+
+        dcc.Tab(label='Adult vector numbers time series', children=[
+
+            html.H2(children='Adult vector time series: ' + wi_name_sh),
+
+            html.Div(children=[
+
+                html.Div(children=[
+                    html.Label(['Outer x-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-xvar3',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rr0'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['Outer y-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-yvar3',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='sne'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['1st sweep var (color):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var3-0',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rc')
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['2nd sweep var (line style):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var3-1',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='d')
+                ], style={'width': '10%'}),
+
+            ], style=dict(display='flex')),
+
+            html.Div([
+                dcc.Graph(id='av-ts',
+                          style={'width': '100%', 'height': '80vh'})
+            ])
+        ]),
+
+        dcc.Tab(label='Infectious vector fraction time series', children=[
+
+            html.H2(children='Infectious vector fraction time series: ' + wi_name_sh),
+
+            html.Div(children=[
+
+                html.Div(children=[
+                    html.Label(['Outer x-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-xvar4',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rr0'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['Outer y-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-yvar4',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='sne'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['1st sweep var (color):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var4-0',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rc')
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['2nd sweep var (line style):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var4-1',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='d')
+                ], style={'width': '10%'}),
+
+            ], style=dict(display='flex')),
+
+            html.Div([
+                dcc.Graph(id='ivf-ts',
+                          style={'width': '100%', 'height': '80vh'})
+            ])
+        ]),
+
+        dcc.Tab(label='Infectious vector numbers time series', children=[
+
+            html.H2(children='Infectious vector time series: ' + wi_name_sh),
+
+            html.Div(children=[
+
+                html.Div(children=[
+                    html.Label(['Outer x-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-xvar5',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rr0'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['Outer y-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-yvar5',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='sne'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['1st sweep var (color):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var5-0',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rc')
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['2nd sweep var (line style):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var5-1',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='d')
+                ], style={'width': '10%'}),
+
+            ], style=dict(display='flex')),
+
+            html.Div([
+                dcc.Graph(id='ivn-ts',
+                          style={'width': '100%', 'height': '80vh'})
+            ])
+        ]),
+
+        dcc.Tab(label='Effector frequency time series', children=[
+
+            html.H2(children='Effector frequency time series: ' + wi_name_sh),
+
+            html.Div(children=[
+
+                html.Div(children=[
+                    html.Label(['Outer x-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-xvar6',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rr0'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['Outer y-var:'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='outer-yvar6',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='sne'
+                    )
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['1st sweep var (color):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var6-0',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='rc')
+                ], style={'width': '10%'}),
+
+                html.Div(children=[
+                    html.Label(['2nd sweep var (line style):'], style={'font-weight': 'bold', 'text-align': 'center'}),
+                    dcc.Dropdown(
+                        id='sweep-var6-1',
+                        options=[{'label': i, 'value': i} for i in list(allvarvals.keys())],
+                        value='d')
+                ], style={'width': '10%'}),
+
+            ], style=dict(display='flex')),
+
+            html.Div([
+                dcc.Graph(id='ef-ts',
+                          style={'width': '100%', 'height': '80vh'})
+            ])
         ])
     ])
-
 ])
 
 
@@ -440,16 +633,124 @@ def update_elim_day_matrices(ov_xvar, ov_yvar, mat_xvar, mat_yvar):
      Input('sweep-var2-0', 'value'),
      Input('sweep-var2-1', 'value')])
 def update_prev_ts(ov_xvar, ov_yvar, svar0, svar1):
-    dfism = dfi[dfi[svar0].isin(allvarvals[svar0]) &
-                dfi[svar1].isin(allvarvals[svar1])]
+    dfinow = dfi[dfi[svar0].isin(allvarvals[svar0]) &
+                 dfi[svar1].isin(allvarvals[svar1]) &
+                 dfi[ov_xvar].isin(allvarvals[ov_xvar]) &
+                 dfi[ov_yvar].isin(allvarvals[ov_yvar])]
 
     allvardefsnow = {k: v for k, v in allvardefs.items() if k not in [svar0, svar1, ov_xvar, ov_yvar]}
-    dfinow = dfism
     for k, v in allvardefsnow.items():
         dfinow = dfinow[dfinow[k] == v]
         dfinow.drop(columns=[k], inplace=True)
 
     fig = px.line(dfinow, x='Time', y='PfHRP2 Prevalence',
+                  labels={
+                      'PfHRP2 Prevalence': ''
+                  },
+                  color=svar0, line_dash=svar1,
+                  facet_col=ov_xvar, facet_row=ov_yvar)
+    return fig
+
+
+@app.callback(
+    Output('av-ts', 'figure'),
+    [Input('outer-xvar3', 'value'),
+     Input('outer-yvar3', 'value'),
+     Input('sweep-var3-0', 'value'),
+     Input('sweep-var3-1', 'value')])
+def update_av_ts(ov_xvar, ov_yvar, svar0, svar1):
+    dfinow = dfi[dfi[svar0].isin(allvarvals[svar0]) &
+                 dfi[svar1].isin(allvarvals[svar1]) &
+                 dfi[ov_xvar].isin(allvarvals[ov_xvar]) &
+                 dfi[ov_yvar].isin(allvarvals[ov_yvar])]
+
+    allvardefsnow = {k: v for k, v in allvardefs.items() if k not in [svar0, svar1, ov_xvar, ov_yvar]}
+    for k, v in allvardefsnow.items():
+        dfinow = dfinow[dfinow[k] == v]
+        dfinow.drop(columns=[k], inplace=True)
+
+    fig = px.line(dfinow, x='Time', y='Adult Vectors',
+                  labels={
+                      'Adult Vectors': '#'
+                  },
+                  color=svar0, line_dash=svar1,
+                  facet_col=ov_xvar, facet_row=ov_yvar)
+    return fig
+
+
+@app.callback(
+    Output('ivf-ts', 'figure'),
+    [Input('outer-xvar4', 'value'),
+     Input('outer-yvar4', 'value'),
+     Input('sweep-var4-0', 'value'),
+     Input('sweep-var4-1', 'value')])
+def update_ivf_ts(ov_xvar, ov_yvar, svar0, svar1):
+    dfinow = dfi[dfi[svar0].isin(allvarvals[svar0]) &
+                 dfi[svar1].isin(allvarvals[svar1]) &
+                 dfi[ov_xvar].isin(allvarvals[ov_xvar]) &
+                 dfi[ov_yvar].isin(allvarvals[ov_yvar])]
+
+    allvardefsnow = {k: v for k, v in allvardefs.items() if k not in [svar0, svar1, ov_xvar, ov_yvar]}
+    for k, v in allvardefsnow.items():
+        dfinow = dfinow[dfinow[k] == v]
+        dfinow.drop(columns=[k], inplace=True)
+
+    fig = px.line(dfinow, x='Time', y='Infectious Vectors',
+                  labels={
+                      'Infectious Vectors': ''
+                  },
+                  color=svar0, line_dash=svar1,
+                  facet_col=ov_xvar, facet_row=ov_yvar)
+    return fig
+
+
+@app.callback(
+    Output('ivn-ts', 'figure'),
+    [Input('outer-xvar5', 'value'),
+     Input('outer-yvar5', 'value'),
+     Input('sweep-var5-0', 'value'),
+     Input('sweep-var5-1', 'value')])
+def update_ivn_ts(ov_xvar, ov_yvar, svar0, svar1):
+    dfinow = dfi[dfi[svar0].isin(allvarvals[svar0]) &
+                 dfi[svar1].isin(allvarvals[svar1]) &
+                 dfi[ov_xvar].isin(allvarvals[ov_xvar]) &
+                 dfi[ov_yvar].isin(allvarvals[ov_yvar])]
+
+    allvardefsnow = {k: v for k, v in allvardefs.items() if k not in [svar0, svar1, ov_xvar, ov_yvar]}
+    for k, v in allvardefsnow.items():
+        dfinow = dfinow[dfinow[k] == v]
+        dfinow.drop(columns=[k], inplace=True)
+
+    fig = px.line(dfinow, x='Time', y='Infectious Vectors Num',
+                  labels={
+                      'Infectious Vectors Num': '#'
+                  },
+                  color=svar0, line_dash=svar1,
+                  facet_col=ov_xvar, facet_row=ov_yvar)
+    return fig
+
+
+@app.callback(
+    Output('ef-ts', 'figure'),
+    [Input('outer-xvar6', 'value'),
+     Input('outer-yvar6', 'value'),
+     Input('sweep-var6-0', 'value'),
+     Input('sweep-var6-1', 'value')])
+def update_ef_ts(ov_xvar, ov_yvar, svar0, svar1):
+    dfanow = dfa[dfa[svar0].isin(allvarvals[svar0]) &
+                 dfa[svar1].isin(allvarvals[svar1]) &
+                 dfa[ov_xvar].isin(allvarvals[ov_xvar]) &
+                 dfa[ov_yvar].isin(allvarvals[ov_yvar])]
+
+    allvardefsnow = {k: v for k, v in allvardefs.items() if k not in [svar0, svar1, ov_xvar, ov_yvar]}
+    for k, v in allvardefsnow.items():
+        dfanow = dfanow[dfanow[k] == v]
+        dfanow.drop(columns=[k], inplace=True)
+
+    fig = px.line(dfanow, x='Time', y=effector_allele,
+                  labels={
+                      effector_allele: '',
+                  },
                   color=svar0, line_dash=svar1,
                   facet_col=ov_xvar, facet_row=ov_yvar)
     return fig
